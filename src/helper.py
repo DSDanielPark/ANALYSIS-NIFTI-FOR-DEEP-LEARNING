4389f44752e9466bd613c0223a82fe6abd01e532
parkminwoo <parkminwoo1991@gmail.com>
2022-06-21 02:09:14 +0900

    feat: update nonzero voxel detect and crop code

--- src/helper.py
+++ src/helper.py
@@ -1,12 +1,12 @@
 import os
 
-def recursive_find_all_files(top_folder_path, file_format):
+def recursive_find_all_files(top_folder_path:str, file_format:str) -> list:
     gathered_file_pathes = []
 
     for (root, directories, files) in os.walk(top_folder_path):
         for file in files:
             if file_format in file:
                 file_path = os.path.join(root, file)
-                print(file_path)
+                gathered_file_pathes.append(file_path)
 
     return gathered_file_pathes
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,7 +8,7 @@ import monai
 # monai cropforeground vs nonzero3darray
 
 
-
+## nonzero3darray
 def if_minus_return_0(value):
     
     if np.sign(value) == -1:
@@ -59,8 +59,48 @@ def makenonzero3darray(nifti_file_path: str, save_path: str, margin: int) -> Non
     
     return nonzero3darray
 
+## for get nonzero 3darray shape from cohort dataset
+def get_nonzero3darray_shape(nifti_file_path: str) -> int:
+    
+    file_name = nifti_file_path.split('\\')[-1].rstrip('nii.gz')
+    
+    nii = nib.load(nifti_file_path)
+    nii_array = nii.get_fdata()
+    original_affine = nii.affine
+    
+    non_zeros = np.nonzero(nii_array>2)
+
+    x_min = non_zeros[0].min()
+    x_max = non_zeros[0].max()
+
+    y_min = non_zeros[1].min()
+    y_max = non_zeros[1].max()
+
+    z_min = non_zeros[2].min()
+    z_max = non_zeros[2].max()
+    
+    
+    
+    return x_min, x_max, y_min, y_max, z_min, z_max
 
 
+## for crop same size when all data(with mask) is co-registerd and preprocessed.
+
+def get_hardcrop(nifti_file_path, save_path, x_min, x_max, y_min, y_max, z_min, z_max):
+    file_name = nifti_file_path.split('\\')[-1].rstrip('nii.gz')
+    
+    nii = nib.load(nifti_file_path)
+    nii_array = nii.get_fdata()
+    nii_array = nii_array[x_min:x_max, y_min:y_max, z_min:z_max]
+    original_affine = nii.affine
+        
+        
+    img = nib.Nifti1Image(nii_array, original_affine) 
+    img.to_filename(f'{save_path}/{file_name}.nii.gz')
+
+    return None
+
+## monai.cropforeground
 def threshold_at_two(x):
     # threshold at 2
     return x > 2
